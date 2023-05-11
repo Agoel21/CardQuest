9425e8b46175062ae55f49e2b360bdabba8b65ac
lnuss2 <89098726+lnuss2@users.noreply.github.com>
2023-05-11 12:13:21 -0500

    Brought over changes to PlayerModel and Crazy8sController from Crazy8s-Console branch.

--- Controllers/Crazy8sController.cs
+++ Controllers/Crazy8sController.cs
@@ -4,7 +4,7 @@
  */
 
 using static CSCE361CardGames.Controllers.CardPileController;
-using static CSCE361CardGames.Controllers.PlayerController;
+using static CSCE361CardGames.Models.PlayerModel;
 using static CSCE361CardGames.Models.CardModel;
 using static CSCE361CardGames.Models.DeckModel;
 
